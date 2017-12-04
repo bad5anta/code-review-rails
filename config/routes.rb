45b0fee05df2504733c287504a0025643a3a73f4
xxx <xxx@gmail.com>
2017-12-04 17:13:31 +0200

    diff update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users
-      resources :diffs, only: %i[index create]
+      resources :diffs, only: %i[index create update]
       resources :stacks, only: %i[index]
       resources :sessions, only: %i[create]
     end
