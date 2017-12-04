4b06fbcdc409c592e11cd734d4c0da41cfa814e2
xxx <xxx@gmail.com>
2017-12-04 14:11:18 +0200

    Added users habtm stacks

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, :name, :role, :password, presence: true
+
+  has_and_belongs_to_many :stacks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204100727) do
+ActiveRecord::Schema.define(version: 20171204120527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stacks", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+  end
+
+  create_table "stacks_users", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "stack_id"
+    t.index ["stack_id"], name: "index_stacks_users_on_stack_id"
+    t.index ["user_id"], name: "index_stacks_users_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
