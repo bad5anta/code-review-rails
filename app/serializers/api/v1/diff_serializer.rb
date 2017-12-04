4c55f1c0dcdb3a2d778d52d10fd510be48c3d387
xxx <xxx@gmail.com>
2017-12-04 15:32:46 +0200

    Added code_changes to the diffs serializer

--- app/serializers/api/v1/diff_serializer.rb
+++ app/serializers/api/v1/diff_serializer.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class DiffSerializer < ActiveModel::Serializer
-      attributes :id
+      attributes :id, :code_changes
 
       belongs_to :author
       belongs_to :reviewer
