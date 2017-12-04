d48111247e35a86b629caacb190ad316d0b09713
xxx <xxx@gmail.com>
2017-12-04 17:11:08 +0200

    jsonapi prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,8 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
+  ActiveModelSerializers.config.adapter = :json_api
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
